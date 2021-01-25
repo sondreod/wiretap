c716ffbfb3bb54bbd2ec11f00212cd4655c0333a
sondreod <sondre_odegard@hotmail.com>
2021-01-25 12:34:07 +0100

    Clean up

--- wiretap/config.py
+++ wiretap/config.py
@@ -1,17 +1,19 @@
 from os import path
 from pydantic import BaseSettings, Field
+from pathlib import Path
 
 
 class Settings(BaseSettings):
-    base_path: str = path.expanduser('~/.config/wiretap/')
+    base_path: str = path.expanduser('~/.config/wiretap')
     pkey_path: str = path.expanduser('~/.ssh/id_rsa')
-    hash_path: str = f'{base_path}seen_hashes'
-    config_file: str = Field('/opt/wiretap/config.json', env='WT_CONFIG_FILE')
-    inventory_file: str = Field(f'{base_path}inventory.json', env='WT_INVENTORY')
+    config_file: str = str(Path(base_path, 'config.json').absolute())
+    inventory_file: str = str(Path(base_path, 'inventory.json').absolute())
+    hash_path: str = str(Path(base_path, 'seen_hashes').absolute())
 
     INFLUX_TOKEN: str = "wE2MepBauGjKasjUy-B4kr74gaUkHuHwg37gcegQksA8zmiWqZ6QwV3t7NsD6-HrD6ljJtIrIJtfMtl2y4Gulg=="
     INFLUX_ORG: str = "test"
     INFLUX_BUCKET: str = "wiretap"
+    INFLUX_HOST: str = "http://localhost:8086"
 
     COLLECTION_INTERVAL: int = Field(60, description="Approx. interval in seconds between metric collection")
     HEALTH_INTERVAL: int = Field(60, description="Approx. interval in seconds between health check")
