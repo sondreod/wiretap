0f14b5d1c3b8087a8e45fcaa0bc1bf39350f222c
sondreod <sondre_odegard@hotmail.com>
2021-01-28 10:06:59 +0100

    Add error handling and naming of threads

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import logging
-import uuid
 import threading
 import time
+import json
 
 from datetime import datetime
 
@@ -15,7 +15,7 @@ from wiretap.config import settings
 from wiretap.remote import Remote
 from wiretap import collectors
 from wiretap.schemas import Metric
-from wiretap.utils import read_file, read_config, write_config, read_inventory
+from wiretap.utils import read_config, read_inventory, get_hashes, set_hashes, append_file
 
 logging.basicConfig(level=logging.ERROR, format='%(asctime)s / %(name)s: %(message)s')
 log = logging.getLogger("root")
@@ -24,17 +24,17 @@ ALL = [collectors.Cpu,
        collectors.Memory,
        collectors.Network,
        collectors.Disk,
-       #collectors.JournalCtl
-       #collectors.Files
+       collectors.JournalCtl,
+       #collectors.Files,
+       #collectors.Logs,
        ]
 
 
 class Wiretap:
 
     def __init__(self):
-        self.hashes = set(read_config('hashes'))
-        self.logs = read_file('logs')
-        self.config = read_config('config')
+        self.hashes = get_hashes()
+        self.config = read_config()
         self.inventory = read_inventory()
         self.metrics = []
         self.client = InfluxDBClient(url=settings.INFLUX_HOST, token=settings.INFLUX_TOKEN)
@@ -57,7 +57,11 @@ class Wiretap:
             return value - last
 
     def add_metric(self, server, metric):
-        measurement, field_name = metric.tag.split('_', 1)
+        measurement = field_name = metric.tag
+        if name_tuple := metric.tag.split('_', 1):
+            if len(name_tuple) == 2:
+                measurement, field_name = name_tuple
+
         if measurement in ['network']:
             metric.value = self.aggregate_diff(server.name+measurement+field_name, metric.value)
             if not metric.value:
@@ -65,13 +69,12 @@ class Wiretap:
         point = Point(measurement) \
             .tag("name", server.name) \
             .tag("agg_type", metric.agg_type) \
-            .field(field_name, float(metric.value)) \
+            .field(field_name, metric.value) \
             .time(datetime.utcfromtimestamp(metric.time), WritePrecision.S)
 
         if self.add_hash(hash(str(point.__dict__))):
             self.add_point_to_db(point)
-        log.error(metric)
-        wri
+            self.metrics.append(metric.json())
 
     def add_hash(self, hash: int):
         """ Returns true if hash is new, otherwise false. New hashes are added to the list"""
@@ -83,30 +86,39 @@ class Wiretap:
     def add_point_to_db(self, point):
         self.write_api.write(settings.INFLUX_BUCKET, settings.INFLUX_ORG, point)
 
+    def append_metrics(self):
+        print(f'Appending {len(self.metrics)} metrics')
+        append_file(settings.metric_file, self.metrics)
+        self.metrics = []
 
 if __name__ == '__main__':
 
     def remote_execution(server, engine):
-
         remote = Remote(server, engine.config)
         collector_objects = ALL
         while True:
             try:
                 for c in collector_objects:
-                    for metric in remote.run(c):
-                        engine.add_metric(server, metric)
+                    if response := remote.run(c):
+                        for metric in response:
+                            engine.add_metric(server, metric)
                 time.sleep(settings.COLLECTION_INTERVAL)
             except SessionError as e:
                 log.error(f"Session error: {e}")
                 time.sleep(900)
                 remote = Remote(server, engine.config)
+            except Exception as e:
+                log.error(f"Error in remote execution. ({server.name}). ({type(e)}) {e}")
 
 
     engine = Wiretap()
     threads = list()
 
     for server in engine.inventory:
-        x = threading.Thread(target=remote_execution, args=(server, engine), daemon=True)
+        x = threading.Thread(target=remote_execution,
+                             args=(server, engine),
+                             name=f"thread_{server.name}",
+                             daemon=True)
         threads.append(x)
         x.start()
 
@@ -116,7 +128,12 @@ if __name__ == '__main__':
             engine.add_metric(server, Metric(tag='health_http_status', time=int(time.time()), value=health_obj.http_status, unit='boolean'))
             engine.add_metric(server, Metric(tag='health_packet_loss', time=int(time.time()), value=health_obj.packet_loss, unit='percent'))
             engine.add_metric(server, Metric(tag='health_rtt', time=int(time.time()), value=health_obj.rtt, unit='ms'))
-            time.sleep(settings.HEALTH_INTERVAL)
+        for thread in threads:
+            if not thread.is_alive():
+                log.error(f"{thread.name} has stopped!")
+        set_hashes(engine.hashes)
+        engine.append_metrics()
+        time.sleep(settings.HEALTH_INTERVAL)
 
     for thread in threads:  # todo: Gracefull join threads?
         thread.join()
