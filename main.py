af47bd23e330c07fc4753c50f87fc2242af85dfe
sondreod <sondre_odegard@hotmail.com>
2021-03-29 15:14:16 +0200

    Refactor main, add sys args

--- main.py
+++ main.py
@@ -1,41 +1,23 @@
-import json
+import sys
 import time
 import logging
 import threading
-import traceback
 
 from datetime import datetime
 
-import uvicorn
-from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
-from fastapi.responses import HTMLResponse
-from pssh.exceptions import SessionError
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 
-from wiretap import collectors
-from wiretap.remote import Remote
+import uvicorn
+
+from wiretap.remote import remote_execution
 from wiretap.schemas import Metric
 from wiretap.config import settings
 from wiretap.health import health_check
-from wiretap.utils import read_config,read_inventory, get_hashes, set_hashes, append_file, check_files,\
-    read_reverse_order, filestats
+from wiretap.utils import read_config, read_inventory, get_hashes, set_hashes, append_file, check_files
 
 logging.basicConfig(level=logging.ERROR, format='%(asctime)s / %(name)s: %(message)s')
 log = logging.getLogger("root")
-
-ALL_COLLECTORS = [
-    collectors.journalctl,
-    collectors.cpu,
-    collectors.memory,
-    collectors.disk,
-    collectors.network,
-    #collectors.Disk,
-    #collectors.JournalCtl,
-    #collectors.Files,
-    #collectors.Logs
-]
 
 
 class Wiretap:
@@ -56,12 +38,23 @@ class Wiretap:
 
         self.diff_lock = threading.Lock()
         self.metric_lock = threading.Lock()
+        self.threads = list()
 
-
-        welcome = ("\nWiretap:\n\nYour inventory:\n")
+        welcome = "\nWiretap:\n\nYour inventory:\n"
         for item in self.inventory:
             welcome += f"   {item}\n"
         log.error(welcome)
+
+        self.start_server_threads()
+
+    def start_server_threads(self):
+        for server in self.inventory:
+            server_thread = threading.Thread(target=remote_execution,
+                                             args=(server, self),
+                                             name=f"thread_{server.name}",
+                                             daemon=True)
+            self.threads.append(server_thread)
+            server_thread.start()
 
 
     def aggregate_diff(self, key, value):
@@ -126,130 +119,48 @@ class Wiretap:
             self.bucket_api.create_bucket(bucket)
         """
 
-app = FastAPI()
-app.mount("/static", StaticFiles(directory="web/static"), name="static")
+def run_configmode():
+    print('Configmode')
 
-@app.get("/", response_class=HTMLResponse)
-def serve_front():
-    with open('web/index.html', 'r') as fd:
-        return str(fd.read())
-
-@app.get("/api/inventory")
-def serve_inventory():
-    inventory = []
-    temp = read_inventory()
-    for item in temp:
-        item = dict(item)
-        item["timestamp"] = 0
-        inventory.append(item)
-
-    for n, line in enumerate(read_reverse_order(settings.metric_file)):
-        if line:
-            line = json.loads(line)
-            if line.get('tag') == 'health_rtt':
-                for server in inventory:
-                    if server.get('name') == line.get('name'):
-                        if line.get('time') > server['timestamp']:
-                            server['timestamp'] = line.get('time')
-                            break
-        if n > 1000 or all(x.get('timestamp') > 0 for x in inventory):
-            break
-
-    return inventory
-
-@app.get("/api/metrics")
-def serve_metrics():
-    for n, line in enumerate(read_reverse_order(settings.metric_file)):
-        if n <= 100 and line:
-            try:
-                yield json.loads(line)
-            except json.JSONDecodeError:
-                pass
-        else:
-            break
-
-@app.get("/api/config")
-def serve_config():
-    return read_config()
-
-
-@app.get("/api/stats")
-def serve_stats():
-    linecount, filesize = map(int, filestats(settings.metric_file))
-    return {
-        "metrics_size": filesize,
-        "metrics_count": '{:,}'.format(linecount).replace(',',' ')
-    }
+def run_webserver():
+    uvicorn.run("web.web:app", host='127.0.0.1', port=1337, workers=4)
 
 if __name__ == '__main__':
 
-    def remote_execution(server, engine):
-        remote = Remote(server, engine.config)
-        epoch = int(time.time())
-        while True:
-            clock = int(time.time()) - epoch
-            try:
-                for c in ALL_COLLECTORS:
-                    interval = 60
-                    if config := engine.config.get(c.__name__.lower()):
-                        interval = int(config.get('interval', 60))
-                    if clock % interval == 0:
-                        for response in remote.run(c):
-                            for metric in response:
-                                print(metric)
-                                engine.add_metric(server, metric)
-
-            except SessionError as e:
-                log.error(f"Session error: {e}")
-                time.sleep(900)
-                remote = Remote(server, engine.config)
-            except Exception as e:
-                log.error(f"Error in remote execution. ({server.name}). ({type(e)}) {e}")
-                log.error(traceback.print_exc())
-                break
-
-            if clock > 3600:
-                epoch = int(time.time())
-
-            time.sleep(.5)
-
-
     engine = Wiretap()
-    threads = list()
-
-    for server in engine.inventory:
-        x = threading.Thread(target=remote_execution,
-                             args=(server, engine),
-                             name=f"thread_{server.name}",
-                             daemon=True)
-        threads.append(x)
-        x.start()
-
 
     def main_loop(engine):
         while True:
-            for server in engine.inventory:
-                health_obj = health_check(server.host)
-                engine.add_metric(server, Metric(tag='health_http_status', time=int(time.time()), value=health_obj.http_status, unit='boolean'))
-                engine.add_metric(server, Metric(tag='health_packet_loss', time=int(time.time()), value=health_obj.packet_loss, unit='%'))
-                if health_obj.rtt:
-                    engine.add_metric(server, Metric(tag='health_rtt', time=int(time.time()), value=health_obj.rtt, unit='ms'))
+            try:
+                for server in engine.inventory:
+                    health_obj = health_check(server.host)
+                    engine.add_metric(server, Metric(tag='health_http_status', time=int(time.time()), value=health_obj.http_status, unit='boolean'))
+                    engine.add_metric(server, Metric(tag='health_packet_loss', time=int(time.time()), value=health_obj.packet_loss, unit='%'))
+                    if health_obj.rtt:
+                        engine.add_metric(server, Metric(tag='health_rtt', time=int(time.time()), value=health_obj.rtt, unit='ms'))
 
-            for thread in threads:
-                if not thread.is_alive():
-                    log.error(f"{thread.name} has stopped!")
-            set_hashes(engine.hashes)
-            engine.append_metrics()
-            for _ in range(10):
-                time.sleep(1)
+                for thread in engine.threads:
+                    if not thread.is_alive():
+                        log.error(f"{thread.name} has stopped!")
+                set_hashes(engine.hashes)
+                engine.append_metrics()
+                for _ in range(10):
+                    time.sleep(1)
+            except KeyboardInterrupt:
+                return
 
     main_loop_thread = threading.Thread(target=main_loop,
                          args=(engine,),
                          name=f"thread_main")
-    threads.append(main_loop_thread)
+    engine.threads.append(main_loop_thread)
+
+
     main_loop_thread.start()
 
-    uvicorn.run("main:app", host='127.0.0.1', port=1337, workers=4)
+    run_options = {'web': run_webserver, 'config': run_configmode}
+    if sys.argv[-1] in run_options.keys():
+        run_options[sys.argv[-1]].__call__()
+
     main_loop_thread.join()
     exit()
 
--- wiretap/remote.py
+++ wiretap/remote.py
@@ -1,11 +1,28 @@
 import logging
 import subprocess
+import time
+import traceback
+
+from pssh.exceptions import SessionError
 from pssh.clients import SSHClient
 
+from wiretap import collectors
 from wiretap.config import settings
 from wiretap.schemas import Server
 
 log = logging.getLogger()
+
+ALL_COLLECTORS = [
+    collectors.journalctl,
+    collectors.cpu,
+    collectors.memory,
+    collectors.disk,
+    collectors.network,
+    collectors.files,
+    #collectors.Disk,
+    #collectors.JournalCtl,
+    #collectors.Logs
+]
 
 
 class Remote:
@@ -14,7 +31,7 @@ class Remote:
     def __init__(self, server: Server, config: dict):
         self.server = server
         self.config = config
-        self._is_localhost = self.server.host in ['localhost', '127.0.0.1']
+        self._is_localhost = self.server.host in ['localhost','127.0.0.1']
 
         if not self._is_localhost:
             self._establish_connection()
@@ -45,3 +62,34 @@ class Remote:
         config = self.config.get(collector.__name__.lower()) or {}
         config['name'] = self.server.name
         return config
+
+
+def remote_execution(server, engine):
+    remote = Remote(server, engine.config)
+    epoch = int(time.time())
+    while True:
+        clock = int(time.time()) - epoch
+        try:
+            for c in ALL_COLLECTORS:
+                interval = 60
+                if config := engine.config.get(c.__name__.lower()):
+                    interval = int(config.get('interval', 60))
+                if clock % interval == 0:
+                    for response in remote.run(c):
+                        for metric in response:
+                            print(metric)
+                            engine.add_metric(server, metric)
+
+        except SessionError as e:
+            log.error(f"Session error: {e}")
+            time.sleep(900)
+            remote = Remote(server, engine.config)
+        except Exception as e:
+            log.error(f"Error in remote execution. ({server.name}). ({type(e)}) {e}")
+            log.error(traceback.print_exc())
+            break
+
+        if clock > 3600:
+            epoch = int(time.time())
+
+        time.sleep(.5)
